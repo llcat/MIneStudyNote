db47c4c4edc3fe3fa9d06a83bd6ba233fa4062f6
llcat <503273361@qq.com>
2017-01-14 11:59:30 +0800

    update show_byte.c

--- docs/CSAPP/show_byte.c
+++ docs/CSAPP/show_byte.c
@@ -4,12 +4,21 @@ typedef unsigned char* byte_pointer;
 
 void show_byte(byte_pointer start,int len){
     int i;
+    printf("0x");
     for(int i = 0;i<len;i++){
         printf("%.2x",start[i]);
     }
     printf("\n");
 }
-
+void show_int(int x){
+    printf("show_int(%d):\n",x);
+    show_byte((byte_pointer)&x,sizeof(x));
+}
+void show_pointer(int *x){
+    printf("指针x的地址:%x\n",&x);
+    printf("show_pointer:\n");
+    show_byte((byte_pointer)&x,sizeof(x));
+}
 int main(){
     int x = 12345;
     int y = 12346;
@@ -19,6 +28,9 @@ int main(){
     printf("p_x : %x\n",p_x);
     printf("y:%d,&y:%x,*(p_x+1):%d\n",y,&y,*(p_x+1));
     printf("p_x+1:%x\n",p_x+1);
+    show_int(x);
+    show_int(y);
+    show_pointer(&x);
     return 0;
 }    
 
