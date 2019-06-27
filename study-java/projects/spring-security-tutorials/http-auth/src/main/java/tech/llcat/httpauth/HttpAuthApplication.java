5d2251ba70ea816bd12457317cfe48eafd6d15c9
pino <17612709402@163.com>
2019-06-27 13:33:36 +0800

    study-java: add http-auth

---
+++ study-java/projects/spring-security-tutorials/http-auth/src/main/java/tech/llcat/httpauth/HttpAuthApplication.java
@@ -0,0 +1,13 @@
+package tech.llcat.httpauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpAuthApplication.class, args);
+	}
+
+}
